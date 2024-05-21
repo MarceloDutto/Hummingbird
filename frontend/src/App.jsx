807eaa8c144c11b149ac84bbfcad4d71b5f9d0fc
Marcelo Dutto <marcelodutto@hotmail.com>
2024-05-21 20:52:24 -0300

    Added Main Content component as wrapper.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
+import MainContent from "./components/main/MainContent.jsx";
+
+
 const App = () => {
   return (
-    <div>
-      Hola mundo
-    </div>
+    <MainContent />
   )
 }
 
---
+++ frontend/src/components/main/MainContent.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import './mainContent.css';
+
+const MainContent = () => {
+    return (
+        <div id="main">
+            <div className="Root">
+                <div className="wrapper">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MainContent;
