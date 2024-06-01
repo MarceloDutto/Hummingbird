d650a3d1278bdf5c2d8125870673a7fc2703b578
Marcelo Dutto <marcelodutto@hotmail.com>
2024-06-01 20:02:59 -0300

    Added 'Not Found' Page.

--- frontend/src/Pages/NotFound/NotFound.jsx
+++ frontend/src/Pages/NotFound/NotFound.jsx
@@ -4,10 +4,24 @@ import React from "react";
 //STYLES
 import './notFound.css';
 
+
+//COMPONENTS
+import Logo from "../../components/Logo/Logo";
+
+
 const NotFound = () => {
     return (
         <div className="NF-wrapper">
-            <h1 className="NF-text" >PAGINA NO ENCONTRADA</h1>
+            <div className="NF-icon">
+                <Logo width={60} height={60} />
+            </div>
+            <div className="NF-content">
+                <h1 className="NF-content-title">No se encontró la página</h1>
+                <p className="NF-content-subtitle">No podemos encontrar la página que buscás</p>
+                <div>
+                    <a href="/" className="text text-body-large-bold NF-home-button">Inicio</a>
+                </div>
+            </div>
         </div>
     )
 };
